eaeb83212619129def15ec6baf0062a08004ba49
davidolufemi <davidayodele64@gmail.com>
2025-05-20 14:49:02 -0400

    git

--- src/main/java/com/example/eksapp/EksApp.java
+++ src/main/java/com/example/eksapp/EksApp.java
@@ -17,7 +17,7 @@ class DemoController {
 
     @GetMapping("/")
     public String home() {
-        return "Hi US Mobile team! This is an automated test! Removed extra service!";
+        return "Testing automated promotion!";
     }
 
     @GetMapping("/health")
